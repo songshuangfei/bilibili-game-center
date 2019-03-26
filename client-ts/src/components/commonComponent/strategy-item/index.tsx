ffcc8faba31528539320f5b67619432afb99018e
songshuangfei <1462499968@qq.com>
2019-03-26 22:38:57 +0800

    移动 strategy item

---
+++ client-ts/src/components/commonComponent/strategy-item/index.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import {ImgLoadingIcon, eyeGrayIcon, goodGrayIcon} from"src/components/icons"
+import "./strategy-item.css";
+import { Link } from 'react-router-dom';
+
+const StrategyItem = (props:{
+    publisherHeadSrc:string,
+    publisherName:string,
+    strategyTitle:string,
+    coverSrc:string,
+    abstract:string,
+    gameName:string,
+    strategyType:string,
+    readNum:string,
+    goodNum:string,
+    strategyId:string
+})=>{
+    return(
+        <div className="common-strategy-item">
+            <div className="publisher-info">
+                <div className="head-pic" style={{backgroundImage:`url(${ImgLoadingIcon})`}}>
+                    <img src={props.publisherHeadSrc} alt=""/>
+                </div>
+                <div className="publisher-name"><span>{props.publisherName}</span></div>
+            </div>
+            <div className="strategy-title">
+                <span>{props.strategyTitle}</span>
+            </div>
+            <div className="strategy-cover" style={{backgroundImage:`url(${ImgLoadingIcon})`}}>
+                <img src={props.coverSrc} alt=""/>
+            </div>
+            <p className="strategy-abstract">
+                <span>
+                    {props.abstract}
+                </span>
+            </p>
+            <div className="strategy-info">
+                <div className="game-and-type">
+                    <span>{props.strategyType}·{props.gameName}</span>
+                </div>
+                <div className="strategy-hot-info">
+                    <img src={eyeGrayIcon} alt=""/>
+                    <span>{props.readNum}</span>
+                    <img className="good-icon" src={goodGrayIcon} alt=""/>
+                    <span>{props.goodNum}</span>
+                </div>
+            </div>
+            <Link to={`/strategyid/${props.strategyId}`}/>
+        </div>
+    )
+}
+
+export default StrategyItem;
