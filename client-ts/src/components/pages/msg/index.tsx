11e97ec92bff91d3a33370f18fc2056341dc751d
songshuangfei <1462499968@qq.com>
2019-04-21 17:40:31 +0800

    delete msg list

--- client-ts/src/components/pages/msg/index.tsx
+++ client-ts/src/components/pages/msg/index.tsx
@@ -1,31 +1,14 @@
 import * as React from 'react';
 import pageScroll from "src/components/commonFunc/scroll";
+import {nomsgIcon} from "src/components/icons";
 import "./msg-page.css";
-import {
-    ListAutoLoadingShowBoard,
-    loadingState
-} from "src/components/commonComponent/list-auto-loading-show-board";
-import MsgList from "./msg-list"
-import {msgApi} from "src/api-request/msg"
 
 class MsgPage extends React.Component {
     public state:{
-        loadingState:loadingState,
-        isLoaded:boolean,
         tabIndex:number,
-        reply:msgItemI[],
-        attitude:msgItemI[],
-        inform:msgItemI[]
     } = {
-        loadingState:loadingState.ready,
-        isLoaded:false,
         tabIndex:0,
-        reply:[],
-        attitude:[],
-        inform:[]
     }
-
-    private tabs = ["reply","attitude","inform"];
 
     public tabTouch(nextIndex:number){
 		if(this.state.tabIndex === nextIndex){
@@ -36,40 +19,13 @@ class MsgPage extends React.Component {
             tabIndex:nextIndex
         })
     }
-    
-    public request = () => {
-        this.setState({
-            loadingState:loadingState.loading
-        });
-
-        const that = this;
-        msgApi(data=>{
-            that.setState({
-                isLoaded:true,
-                loadingState:loadingState.ready,
-                reply:data.reply,
-                attitude:data.attitude,
-                inform:data.inform
-            })
-        })
-    }
-
-    public componentDidMount() {
-        this.request();
-    }
 
     public render(){
         const tabNowIndex = this.state.tabIndex;
         const markLeft = 33.3*tabNowIndex + 16.7 +"%";
         return(
-            <div>
-                <div className="msg-content">
-                    {
-                        this.state.isLoaded?
-                        <MsgList msgs={this.state[this.tabs[tabNowIndex]]}/>:
-                        <ListAutoLoadingShowBoard now={this.state.loadingState} failedRetry={this.request}/>
-                    }
-                </div>
+            <div className="msg-page">
+                <img className="no-msg-icon"src={nomsgIcon} alt=""/>
                 <div className="msg-type-tabs">
                     <div className={`tab ${tabNowIndex===0?"tab-active":''}`}  onClick={()=>{this.tabTouch(0)}}>回复</div>
                     <div className={`tab ${tabNowIndex===1?"tab-active":''}`} onClick={()=>{this.tabTouch(1)}}>态度</div>
