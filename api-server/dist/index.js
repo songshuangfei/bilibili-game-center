197dec6b88f4a4800abf311e465127ab912dc512
songshuangfei <1462499968@qq.com>
2019-04-22 13:33:20 +0800

    new api server(koa)

--- api-server/dist/index.js
+++ api-server/dist/index.js
@@ -1,2 +1,10 @@
-console.log("宋");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Koa = require("koa");
+const routes_1 = require("./routes");
+const app = new Koa();
+/*启动路由*/
+app.use(routes_1.default.routes())
+    .use(routes_1.default.allowedMethods());
+app.listen(8000);
 //# sourceMappingURL=index.js.map
--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -1,1 +1,9 @@
-console.log("宋")
+import * as Koa from "koa";
+import router from "./routes"
+const app=new Koa();
+
+/*启动路由*/
+app.use(router.routes())   
+    .use(router.allowedMethods());   
+
+app.listen(8000);
