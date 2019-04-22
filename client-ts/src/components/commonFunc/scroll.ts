4f40a901e0fbf0bb4c18d9dcc4514e8adb8cc7e5
songshuangfei <1462499968@qq.com>
2019-04-22 12:42:38 +0800

    scroller

--- client-ts/src/components/commonFunc/scroll.ts
+++ client-ts/src/components/commonFunc/scroll.ts
@@ -1,27 +1,30 @@
 const pageScroll = {
     _value:{
         find:0,
-        home:0,
+        home:10,
         my:0,
         rank:0,
     },
 
     _resetPageScroll():void{
         document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
     },
 
     saveScrollTop(pageName:string):void{
-        const scrollTop:number = document.documentElement.scrollTop;
+        const scrollTop:number = document.documentElement.scrollTop || document.body.scrollTop;
         this._value[pageName] = scrollTop;
         this._resetPageScroll();
     },
    
     setScrollTopToPage(pageName:string):void{
         document.documentElement.scrollTop = this._value[pageName];
+        document.body.scrollTop = this._value[pageName];
     },
 
     setPageScrollWithValue(v:number){
-        document.documentElement.scrollTop = 0;
+        document.documentElement.scrollTop = v;
+        document.body.scrollTop  = v;
     }
 }
 
--- client-ts/src/components/commonFunc/scrollMonitor.ts
+++ client-ts/src/components/commonFunc/scrollMonitor.ts
@@ -3,7 +3,7 @@ const  scrollMonitor = {
         function monitor(){
             const viewPortHeight = document.documentElement.clientHeight;
             const contentHeight = document.documentElement.scrollHeight;
-            const scrollerTop = document.documentElement.scrollTop;
+            const scrollerTop = document.documentElement.scrollTop || document.body.scrollTop;
             if(viewPortHeight + scrollerTop >= contentHeight - 100){
                 // 接近总长度的80%触发
                 // 移动端的上下拖动页面高度会变化，所以阈值设置较小为0.8
