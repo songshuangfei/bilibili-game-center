26a9882b31dc9543041ad2cbc552c0ea22781365
songshuangfei <1462499968@qq.com>
2019-09-10 12:05:59 +0800

    路由懒加载

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,36 +1,61 @@
 import * as React from 'react';
+import { Suspense } from 'react';
 import { Route, Switch, } from 'react-router-dom';
 import "./app.css";
 import Nav from "./components/nav";
 import TitleBar from "./components/titleBar";
 
-import Home from "./components/pages/home";
-import Rank from "./components/pages/rank";
-import Find from "./components/pages/find";
-import Strategy from "./components/pages/strategy";
-import My from "./components/pages/my";
+// import Home from "./components/pages/home";
+// import Rank from "./components/pages/rank";
+// import Find from "./components/pages/find";
+// import Strategy from "./components/pages/strategy";
+// import My from "./components/pages/my";
 
-import Search from "./components/pages/search/";
-import GamePage from './components/pages/gamePage';
-import MsgPage from "./components/pages/msg";
-import MyDownload  from "./components/pages/my-download";
-import NotFound from "./components/pages/notFound";
-import Setting from "./components/pages/setting"
+// import Search from "./components/pages/search/";
+// import GamePage from './components/pages/gamePage';
+// import MsgPage from "./components/pages/msg";
+// import MyDownload  from "./components/pages/my-download";
+// import NotFound from "./components/pages/notFound";
+// import Setting from "./components/pages/setting"
 
+
+
+const Home = React.lazy(() => import("./components/pages/home"));
+const Rank = React.lazy(() => import("./components/pages/rank"));
+const Find = React.lazy(() => import("./components/pages/find"));
+const Strategy = React.lazy(() => import("./components/pages/strategy"));
+const My = React.lazy(() => import("./components/pages/my"));
+const Search = React.lazy(() => import("./components/pages/search"));
+const GamePage = React.lazy(() => import("./components/pages/gamePage"));
+const MsgPage = React.lazy(() => import("./components/pages/msg"));
+
+const MyDownload = React.lazy(() => import("./components/pages/my-download"));
+const NotFound = React.lazy(() => import("./components/pages/notFound"));
+const Setting = React.lazy(() => import("./components/pages/setting"));
+
+
+function AsyncComponent(props:{Ac:any,route:any}){
+	const { Ac, route } = props;
+	return(
+		<Suspense fallback={<div>Loading...</div>}>
+			<Ac {...route}/>
+		</Suspense>
+	)
+}
 function MainPagesWrapper(){
 	const pathNow:string = window.location.pathname;
 		const isManPage:boolean = ['/','/rank','/find','/strategy','/my'].indexOf(pathNow)===-1?false:true; 
 	return (
 		<div className="title-pages-wrapper">
 			<Switch>
-				<Route exact={true} path="/" component={Home}/>
-				<Route path="/rank" component={Rank}/>
-				<Route path="/find" component={Find}/>
-				<Route path="/strategy" component={Strategy}/>
-				<Route path="/my" component={My}/>
-				<Route path="/msg" component={MsgPage} />
-				<Route path="/mydownload" component={MyDownload} />
-				<Route path="/setting" component={Setting} />
+				<Route exact={true} path="/" component={(route:any)=><AsyncComponent Ac={Home} route={route} />}/>
+				<Route path="/rank" component={(route:any)=><AsyncComponent Ac={Rank} route={route} />}/>
+				<Route path="/find" component={(route:any)=><AsyncComponent Ac={Find} route={route} />}/>
+				<Route path="/strategy" component={(route:any)=><AsyncComponent Ac={Strategy} route={route} />}/>
+				<Route path="/my" component={(route:any)=><AsyncComponent Ac={My} route={route} />}/>
+				<Route path="/msg" component={(route:any)=><AsyncComponent Ac={MsgPage} route={route} />}/>
+				<Route path="/mydownload" component={(route:any)=><AsyncComponent Ac={MyDownload} route={route} />} />
+				<Route path="/setting" component={(route:any)=><AsyncComponent Ac={Setting} route={route} />}/>
 			</Switch>
 			<TitleBar/>
 			{isManPage?<Nav/>:""}
@@ -42,9 +67,9 @@ function OtherPagesWrapper(){
 	return (
 		<div className="no-title-pages-wrapper">
 			<Switch>
-				<Route path="/search" component={Search}/>
-				<Route path="/game" component={GamePage} />
-				<Route path="*" component={NotFound} />
+				<Route path="/search" component={(route:any)=><AsyncComponent Ac={Search} route={route}/>}/>
+				<Route path="/game" component={(route:any)=><AsyncComponent Ac={GamePage} route={route}/>} />
+				<Route path="*" component={(route:any)=><AsyncComponent Ac={NotFound} route={route}/>}/>
 			</Switch>
 		</div>
 	)
